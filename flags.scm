8d851f782fd818cfc8fd57386ae7ab9514a67fbd
andytilia <andytilia@gmail.com>
2017-08-14 14:47:30 -0400

    set up web view

--- flags.scm
+++ flags.scm
@@ -1,9 +1,30 @@
+#|
+
+YOUR-REAL-NAME
+YOUR-GITHUB-USERNAME: LINK-TO-YOUR-CLASS-PROFILE
+ASSIGNMENT-NAME: LINK-TO-ASSIGNMENT-ON-GITHUB-PAGES
+CURRENT-MONTH CURRENT-YEAR
+
+answers to lab questions
+1.
+2.
+3.
+...
+
+|#
+
 ; a blank flag is a 300x200 rectangle, which is outlined in black
-; 1) start with a red dot, of radius 50
+
+; start with a red dot, of radius 50
 (define dot (circle 50 "solid" "red"))
-; 2) define a variable called "blank", which is a 300x200, outlined black rectangle
+
+; then define a variable called "blank", which is a 300x200, outlined black rectangle
 (define blank (rectangle 300 200 "outline" "black"))
-; 3) define "japan" to be the flag of japan (a red dot, centered on a blank rectangle)
+
+; then define "japan" to be the flag of japan (a red dot, centered on a blank rectangle)
 (define japan (put-image dot
                          150 100
                          blank))
+
+; finally, I'll print the "japan" variable
+japan
